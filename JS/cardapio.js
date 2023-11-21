ae096b91009ee401b6b6139197dfb1427f2b0070
João Marcelo <145791198+jmbrex@users.noreply.github.com>
2023-11-20 22:35:35 -0300

    Update cardapio.js
    testando novo caminho popUp.html

--- JS/cardapio.js
+++ JS/cardapio.js
@@ -1,6 +1,6 @@
 var win;
 function PopUpPromocao(){
-    pagina="../popUp.html";
+    pagina="popUp.html";
     var left = (window.innerWidth-900)/2;
     var top = (window.innerHeight-600)/2;
     settings = 'toolbar=no, location=no,directories=no,status=no,menubar=no,width=400,height=420,left='+left+',top='+top+'scrollbars=no,copyhistory=yes';
